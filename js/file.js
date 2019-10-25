7e633310a5c97d33692fe49582a8d9839b8c966b
Juan Mora Serrano <tocheserrano1@gmail.com>
2019-10-25 11:08:22 -0600

    first commit

---
+++ js/file.js
@@ -0,0 +1,60 @@
+var file = (function($) {
+  var init = function() {
+    const date = document.querySelector("#date");
+    const newDate = new Date();
+    date.innerHTML = `${newDate.getDate()}/${newDate.getMonth() +
+      1}/${newDate.getFullYear()} `;
+  };
+
+  var eventHandlers = function() {
+    const ipad = window.matchMedia("screen and (max-width: 767px)");
+    ipad.addListener(validation);
+
+    const menu = document.querySelector("#menu");
+    const burgerButton = document.querySelector("#burger-menu");
+
+    const btnSendEmail = document.querySelector("#btnSendEmail");
+    const email = document.querySelector("#email");
+
+    menu.addEventListener("click", hideShow);
+
+    btnSendEmail.addEventListener("click", prepareEmail);
+
+    function validation(event) {
+      if (event.matches) {
+        burgerButton.addEventListener("click", hideShow);
+      } else {
+        burgerButton.removeEventListener("click", hideShow);
+      }
+    }
+
+    validation(ipad);
+
+    function hideShow() {
+      if (menu.classList.contains("is-active")) {
+        menu.classList.remove("is-active");
+      } else {
+        menu.classList.add("is-active");
+      }
+    }
+
+    function prepareEmail() {
+      event.preventDefault();
+      if (email.value != undefined && email.value != "") {
+        window.open(
+          "mailto:tocheserrano1@gmail.com?subject=Contacto&body=" + email.value
+        );
+      }
+    }
+  };
+
+  return {
+    init: init,
+    eventHandlers: eventHandlers
+  };
+})(jQuery);
+
+$(document).ready(function() {
+  file.init();
+  file.eventHandlers();
+});
